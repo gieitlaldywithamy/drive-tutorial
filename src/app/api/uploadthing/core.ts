e6676009f0fbeec34622dd9105815e7a77a423f3
amy-plt <amy.morrison@prettylittlething.com>
2025-02-18 18:40:09 +0000

    ignore custom error

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -30,12 +30,14 @@ export const ourFileRouter = {
       const user = await auth();
 
       // If you throw, the user will not be able to upload
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
       const folder = await QUERIES.getFolderById(input.folderId);
-
+      // eslint-disable-next-line @typescript-eslint/only-throw-error
       if (!folder) throw new UploadThingError("Folder not found");
       if (folder.ownerId !== user.userId)
+        // eslint-disable-next-line @typescript-eslint/only-throw-error
         throw new UploadThingError("You are not authorised");
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.userId, folderId: folder.id };
